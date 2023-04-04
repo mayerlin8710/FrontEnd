b557b06c59b972102b4405797fac6125c1892fa8
Mayerlin <mayerlin8710@gmail.com>
2023-04-04 10:50:47 -0400

    tarea 7

---
+++ Modulo 2/session7/assets/js/logica.js
@@ -0,0 +1,93 @@
+// crearemos nuestro primer hola mundo desde el scroipt
+//console.log ("hola mundo desde js");
+
+//var numero1=5; // js entiende que un numero es de tipo number
+//var numero2=10;
+//var nombre= "Jose";
+//var resultado= numero1 =numero2;
+
+// console.log(nombre =" dice que el resultado de la suma es: :"=resultado);
+var numero1, numero2, resultado;// estamos creando 3 variables nulas a la vez
+let opcion = 0;// el salto de linea se hace con la tecla \n
+opcion = parseInt(prompt("seleccione que desea hacer:\n1.Calcular cuál número es mayor \n2.Sumar \n3.Restar\n4.Multiplicar\n5.Dividir\n6. Mostrar los números ingresados\n7 Salir"));
+console.log(opcion);
+if (opcion < 1 || opcion >7) {
+    alert("Eleccion Incorrecta. Adiós ");
+}
+else {
+    numero1 = prompt("ingrese el primer número");
+    numero2 = prompt("ingrese el segundo número");
+    numero1 = parseInt(numero1);
+    numero2 = parseInt(numero2);
+    switch (opcion) {
+        case 1:
+            if (numero1 > numero2) {
+                alert("En Primer número es mayor ");
+            } else if (numero1 === numero2) {
+                alert("Los números son iguales");
+            }
+            else {
+                alert("En segundo número es mayor ");
+            }
+            break;
+        case 2:
+            resultado = suma(numero1, numero2);
+            alert("El resultado de la suma es " + resultado);
+            break;
+        case 3:
+            resultado = resta(numero1, numero2);
+            alert("El resultado de la suma es " + resultado);
+            break;
+        case 4:
+            resultado = mult(numero1, numero2);
+            alert("El resultado de la Multiplicación es " + resultado);
+            break;
+        case 5:
+            resultado = div(numero1, numero2);
+            alert("El resultado de la división es " + resultado);
+            break;
+        case 6:
+            alert("Usted ha ingresado  " + numero1 + "  y " + numero2)
+            break;
+        case 7:
+            alert("Adiós");
+            break;
+    }
+
+
+
+    //resultado=numero1+numero2;
+    //alert("la suma de "+ numero1+ "+" + numero2 + "es" + resultado)
+    //console.log("el resultado es"+resultado);
+
+    function suma(numero1, numero2) {
+        let suma = numero1 + numero2;
+        return suma
+
+    }
+
+
+    function resta(numero1, numero2) {
+        let resta = numero1 - numero2
+        return resta
+
+
+    }
+
+    function div(numero1, numero2) {
+        let div = numero1 / numero2;
+        return div
+
+    }
+
+    function mult(numero1, numero2) {
+        let mult = numero1 * numero2
+        return mult
+
+
+    }
+
+    alert("Programa Finalizado")
+
+    //alert("la suma de " + numero1 + "+" + numero2 + "es" + resultado);
+}
