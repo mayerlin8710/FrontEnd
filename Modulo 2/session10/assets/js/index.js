87f8ee6dbe59a0db631297244e0d409c4d9614a7
Mayerlin <mayerlin8710@gmail.com>
2023-04-09 13:00:51 -0400

    session 4 ejercicio

---
+++ Modulo 2/session10/assets/js/index.js
@@ -0,0 +1,4 @@
+$(document).ready(function () {
+    var miTexto = $("#contenido").text();
+alert(miTexto);
+})
---
+++ Modulo 2/session8/assets\js/logica.js
@@ -0,0 +1,26 @@
+// ciclo for
+
+var numero = parseInt(prompt("Ingrese un número"));
+//inicio++ es igual a sumarle uno en terminos de codigo inicio = inicio + 1 //= 100
+
+for(var inicio = 1; inicio <= numero; inicio ++){
+ console.log(inicio);
+}
+// segundo ciclo inicio 2, 2<=2 100 entonces ocurre la accion y fnailmente inicio = inicio+1 
+
+
+//** 
+
+
+while (numero <10)) {
+    
+
+    
+}
+
+
+
+
+
+
+
